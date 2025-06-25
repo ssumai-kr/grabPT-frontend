4f8aa841a35553961eec7af0190b075199e34d77
leetaesk <itasuk@naver.com>
2025-06-25 16:03:25 +0900

    chore: zustand 설치

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,8 +1,17 @@
+import { useBearStore } from '@/store/useStore';
+
 function Test() {
+  const { bears, increase } = useBearStore();
+
   return (
-    <div className="flex w-full items-center justify-center">
-      <button className="mt-2 cursor-pointer rounded bg-pink-300 p-2 text-black transition-all duration-200 hover:bg-pink-500">
-        테스트버튼
+    <div className="flex w-full flex-col items-center justify-center gap-2">
+      <p className="text-lg text-black">곰의 수: {bears}</p>
+
+      <button
+        onClick={() => increase(1)}
+        className="rounded bg-pink-300 px-4 py-2 text-black transition-colors duration-200 hover:bg-pink-500"
+      >
+        곰 +1
       </button>
     </div>
   );
