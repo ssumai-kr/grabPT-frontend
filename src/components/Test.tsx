b7187f96a1aa6bc0bdbcd1e2fbe128c974d89c41
leetaesk <itasuk@naver.com>
2025-06-25 15:25:04 +0900

    chore: axios 설치

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,7 @@
 function Test() {
   return (
     <div className="flex w-full items-center justify-center">
-      <button className="mt-2 cursor-pointer rounded bg-white p-2 text-black hover:bg-pink-500">
+      <button className="mt-2 cursor-pointer rounded bg-pink-300 p-2 text-black transition-all duration-200 hover:bg-pink-500">
         테스트버튼
       </button>
     </div>
