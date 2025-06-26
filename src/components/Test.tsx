44f33482af2faf722b4f41e214ab39304fe43ba5
leetaesk <itasuk@naver.com>
2025-06-26 10:25:02 +0900

    chore: test코드 삭제

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,17 +1,8 @@
-import { useBearStore } from '@/store/useStore';
-
 function Test() {
-  const { bears, increase } = useBearStore();
-
   return (
     <div className="flex w-full flex-col items-center justify-center gap-2">
-      <p className="text-lg text-black">곰의 수: {bears}</p>
-
-      <button
-        onClick={() => increase(1)}
-        className="rounded bg-pink-300 px-4 py-2 text-black transition-colors duration-200 hover:bg-pink-500"
-      >
-        곰 +1
+      <button className="cursor-pointer rounded bg-pink-300 px-4 py-2 text-black transition-colors duration-200 hover:bg-pink-500">
+        Test
       </button>
     </div>
   );
