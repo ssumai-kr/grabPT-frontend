8348ef1a916660dbda27e9db657d8688107e428d
leetaesk <itasuk@naver.com>
2025-07-15 02:32:38 +0900

    fix:layout-clipping

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import Footer from '@/layout/components/Footer';
@@ -5,20 +6,19 @@ import Header from '@/layout/components/Header';
 
 function Layout() {
   const location = useLocation();
-
-  // 전체화면으로 보여야 할 경로 리스트
-  const fullWidthRoutes = ['/'];
-  const isFullWidthPage = fullWidthRoutes.includes(location.pathname);
+  const isFullWidthPage = location.pathname === '/';
 
   return (
     <div className="flex h-screen flex-col">
-      {/* Header */}
       <Header />
 
-      <section className="flex flex-1 flex-col justify-between overflow-y-scroll">
-        <main className={`${isFullWidthPage ? '' : 'px-[220px] max-[1440px]:px-[180px]'}`}>
+      {/* 스크롤시 section내부요소가 잘리는 버그 발생. => GPU/페인팅 조작으로 일단 해결 */}
+      {/* 뭐가 문젠지 아직도 잘 모르겠는데 일단 해결함;;; */}
+      <section className="flex min-h-0 flex-1 basis-0 [transform:translateZ(0)] flex-col overflow-y-scroll [will-change:transform] [contain:layout_paint]">
+        <main className={clsx(!isFullWidthPage && 'px-[220px] max-[1440px]:px-[180px]')}>
           <Outlet />
         </main>
+
         <Footer />
       </section>
     </div>
