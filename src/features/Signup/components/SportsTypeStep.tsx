909fb8e8a3e9e8eb7a19c5c0f56606869d344cf6
leetaesk <itasuk@naver.com>
2025-08-02 00:09:09 +0900

    fix:onBoarding

--- src/features/Signup/components/SportsTypeStep.tsx
+++ src/features/Signup/components/SportsTypeStep.tsx
@@ -21,27 +21,25 @@ const SportsTypeStep = ({ onNext }: SportTypeStepProps) => {
     onNext();
   };
   return (
-    <div className="flex flex-col">
-      <div className="mt-32 mb-72 flex flex-col items-center justify-center">
-        <div className="mb-27 flex items-center justify-center text-5xl font-extrabold whitespace-pre">
-          <span>원하는 </span>
-          <span className="text-[color:var(--color-button)]">운동 종목</span>
-          <span>을 선택해주세요</span>
-        </div>
+    <div className="flex flex-col items-center justify-center">
+      <div className="mb-27 flex items-center justify-center text-5xl font-extrabold whitespace-pre">
+        <span>원하는 </span>
+        <span className="text-[color:var(--color-button)]">운동 종목</span>
+        <span>을 선택해주세요</span>
+      </div>
 
-        {/* 공용 셀렉터 컴포넌트 */}
+      {/* 공용 셀렉터 컴포넌트 */}
 
-        <SportsTypeSelector
-          value={SPORTS.find((s) => s.id === sportsTypeInfo.categoryId) ?? null}
-          onChange={handleSelect}
-        />
+      <SportsTypeSelector
+        value={SPORTS.find((s) => s.id === sportsTypeInfo.categoryId) ?? null}
+        onChange={handleSelect}
+      />
 
-        <div className="mx-[32rem] mt-[8.3rem] w-[25.5625rem]">
-          {/* 아무 것도 안 골랐을 때는 비활성화 */}
-          <SignupBtn disabled={!sportsTypeInfo.categoryId} onClick={handleNext}>
-            다음
-          </SignupBtn>
-        </div>
+      <div className="mx-[32rem] mt-[8.3rem] w-[25.5625rem]">
+        {/* 아무 것도 안 골랐을 때는 비활성화 */}
+        <SignupBtn disabled={!sportsTypeInfo.categoryId} onClick={handleNext}>
+          다음
+        </SignupBtn>
       </div>
     </div>
   );
--- src/features/Signup/components/UserTypeStep.tsx
+++ src/features/Signup/components/UserTypeStep.tsx
@@ -25,72 +25,70 @@ const UserTypeStep = ({ onNext }: UserTypeStepProps) => {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="mt-32 mb-72 flex flex-col items-center justify-center">
-        <div className="flex items-center justify-center text-5xl font-extrabold whitespace-pre">
-          <span>어떤 유형의 </span>
-          <span className="text-[#003EFB]">사용자</span>
-          <span> 인가요?</span>
-        </div>
-        <div className="mt-8 flex items-center justify-center text-2xl font-semibold text-[#646678]">
-          Grab PT를 어떻게 사용할지 선택해 주세요.
-        </div>
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center text-5xl font-extrabold whitespace-pre">
+        <span>어떤 유형의 </span>
+        <span className="text-[#003EFB]">사용자</span>
+        <span> 인가요?</span>
+      </div>
+      <div className="mt-8 flex items-center justify-center text-2xl font-semibold text-[#646678]">
+        Grab PT를 어떻게 사용할지 선택해 주세요.
+      </div>
 
-        {/* 카드 */}
-        <div className="mx-auto mt-24 flex w-4xl justify-center gap-20">
-          {/* 일반 이용자 카드 */}
-          <div
-            className="w-96 transform rounded-[1.25rem] shadow-2xl transition-transform duration-200 ease-in-out hover:scale-105"
-            onClick={() => handleSelect('normal')}
-          >
-            <div className="relative cursor-pointer overflow-hidden rounded-[1.25rem]">
-              <img
-                src={SignupforUser}
-                alt="일반 이용자 사진"
-                className="z-0 h-auto w-full object-cover"
-              />
+      {/* 카드 */}
+      <div className="mx-auto mt-24 flex w-4xl justify-center gap-20">
+        {/* 일반 이용자 카드 */}
+        <div
+          className="w-96 transform rounded-[1.25rem] shadow-2xl transition-transform duration-200 ease-in-out hover:scale-105"
+          onClick={() => handleSelect('normal')}
+        >
+          <div className="relative cursor-pointer overflow-hidden rounded-[1.25rem]">
+            <img
+              src={SignupforUser}
+              alt="일반 이용자 사진"
+              className="z-0 h-auto w-full object-cover"
+            />
 
-              <div
-                className={`absolute inset-0 transition-colors duration-200 ease-in-out ${role !== 1 ? 'bg-black/58' : 'bg-[#003EFBB2]/50'}`}
-              />
+            <div
+              className={`absolute inset-0 transition-colors duration-200 ease-in-out ${role !== 1 ? 'bg-black/58' : 'bg-[#003EFBB2]/50'}`}
+            />
 
-              <div className="absolute top-6 left-6 z-10 text-center font-semibold text-white">
-                PT를 수강하고 싶어요!
-              </div>
-              <div className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform text-4xl font-bold whitespace-nowrap text-white">
-                일반 이용자 가입
-              </div>
+            <div className="absolute top-6 left-6 z-10 text-center font-semibold text-white">
+              PT를 수강하고 싶어요!
             </div>
-          </div>
-
-          {/* 전문가 카드 */}
-          <div
-            className="w-96 transform rounded-[1.25rem] shadow-2xl transition-transform duration-200 ease-in-out hover:scale-105"
-            onClick={() => handleSelect('expert')}
-          >
-            <div className="relative cursor-pointer overflow-hidden rounded-[1.25rem]">
-              <img
-                src={SignupforTrainer}
-                alt="전문가 트레이너 사진"
-                className="z-0 h-auto w-full object-cover"
-              />
-              <div
-                className={`absolute inset-0 transition-colors duration-200 ease-in-out ${role !== 2 ? 'bg-black/58' : 'bg-[#003EFBB2]/50'}`}
-              />
-
-              <div className="absolute top-6 left-6 z-10 font-semibold text-white">
-                PT를 수강생을 모집하고 싶어요!
-              </div>
-              <div className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform text-center text-4xl font-bold whitespace-nowrap text-white">
-                전문가 트레이너 가입
-              </div>
+            <div className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform text-4xl font-bold whitespace-nowrap text-white">
+              일반 이용자 가입
             </div>
           </div>
         </div>
-        {/* 다음 버튼 */}
-        <div className="mx-[32rem] mt-[8.5rem] w-[25.5625rem]">
-          <SignupBtn onClick={handleNext}>다음</SignupBtn>
+
+        {/* 전문가 카드 */}
+        <div
+          className="w-96 transform rounded-[1.25rem] shadow-2xl transition-transform duration-200 ease-in-out hover:scale-105"
+          onClick={() => handleSelect('expert')}
+        >
+          <div className="relative cursor-pointer overflow-hidden rounded-[1.25rem]">
+            <img
+              src={SignupforTrainer}
+              alt="전문가 트레이너 사진"
+              className="z-0 h-auto w-full object-cover"
+            />
+            <div
+              className={`absolute inset-0 transition-colors duration-200 ease-in-out ${role !== 2 ? 'bg-black/58' : 'bg-[#003EFBB2]/50'}`}
+            />
+
+            <div className="absolute top-6 left-6 z-10 font-semibold text-white">
+              PT를 수강생을 모집하고 싶어요!
+            </div>
+            <div className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform text-center text-4xl font-bold whitespace-nowrap text-white">
+              전문가 트레이너 가입
+            </div>
+          </div>
         </div>
+      </div>
+      {/* 다음 버튼 */}
+      <div className="mx-[32rem] mt-[8.5rem] w-[25.5625rem]">
+        <SignupBtn onClick={handleNext}>다음</SignupBtn>
       </div>
     </div>
   );
