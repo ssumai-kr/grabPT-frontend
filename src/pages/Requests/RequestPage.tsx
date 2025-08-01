32cd0f9c18fe0f11d0c2853e5a23892c20438f03
leetaesk <itasuk@naver.com>
2025-08-02 01:28:14 +0900

    fix:~request

--- src/pages/Requests/RequestPage.tsx
+++ src/pages/Requests/RequestPage.tsx
@@ -1,7 +1,10 @@
 import { useCallback } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import Button from '@/components/Button';
 import Stepper from '@/components/Stepper';
+import { urlFor } from '@/constants/routes';
 import FillDetailStep from '@/features/Request/components/FillDetailStep';
 import SelectPriceStep from '@/features/Request/components/SelectPriceStep';
 import SelectSportStep from '@/features/Request/components/SelectSportStep';
@@ -14,25 +17,35 @@ const STEP_MAP = {
 } as const;
 
 const RequestPage = () => {
+  const navigate = useNavigate();
   /* step 관리 훅 */
   const { step, next, isLast } = useStepParam(3);
 
   /* 다음 버튼 핸들러 */
   const handleNextStep = useCallback(() => {
-    if (isLast) alert('요청서 제출 완료');
+    if (isLast) {
+      alert('요청서 제출 완료');
+      // post요청 보내고 id받아와서 해당 요청서 상세페이지로 리다이렉트
+      // 일단 하드코딩
+      navigate(urlFor.requestDetail(3));
+    }
 
     next(); // 쿼리 파라미터를 step+1 로 수정
-  }, [isLast, next]);
+  }, [isLast, next, navigate]);
 
   const StepComponent = STEP_MAP[step];
 
   return (
-    <div className="box-border flex flex-col items-center gap-8 py-12 text-center">
+    <div className="box-border flex flex-col items-center py-[100px] text-center">
       <StepComponent />
-      <Button onClick={handleNextStep} width="w-[26rem]">
-        {isLast ? '작성 완료' : '다음'}
-      </Button>
-      <Stepper total={3} current={step} />
+      <div className="mt-12">
+        <Button onClick={handleNextStep} width="w-[26rem]">
+          {isLast ? '작성 완료' : '다음'}
+        </Button>
+      </div>
+      <div className="mt-[55px]">
+        <Stepper total={3} current={step} />
+      </div>
     </div>
   );
 };
