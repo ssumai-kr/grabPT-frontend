c7521c5fde5329bb713dacb3a0931a0edae32fd4
Starving_Orange <kyungjjoo@gmail.com>
2025-08-15 19:31:10 +0900

    fix(RequestDetail):타입 수정

--- src/pages/Requests/RequestDetailPage.tsx
+++ src/pages/Requests/RequestDetailPage.tsx
@@ -183,7 +183,7 @@ const RequestDetailPage = () => {
         <img
           src={data?.profileImageUrl}
           alt="요청서 프로필"
-          className="h-full"
+          className="h-[3.125rem] w-[3.125rem] rounded-full"
           onError={onErrorImage}
         />
         <span className="text-4xl font-extrabold">
