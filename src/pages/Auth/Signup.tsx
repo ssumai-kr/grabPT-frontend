4ea64ce3d4fdd12aea3a13b619baea2ff03545e8
Starving_Orange <kyungjjoo@gmail.com>
2025-08-18 02:22:02 +0900

    회원가입 리다이렉트 재설정

--- src/pages/Auth/Signup.tsx
+++ src/pages/Auth/Signup.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
-import ROUTES from '@/constants/routes';
 import BackBtn from '@/features/Signup/assets/BackBtn.svg';
 import AgreementStep from '@/features/Signup/components/AgreementStep';
 import ExpertInfoStep from '@/features/Signup/components/ExpertInfoStep';
@@ -68,7 +67,7 @@ const Signup = () => {
           {
             onSuccess: (res) => {
               console.log('User signup success:', res);
-              nav(ROUTES.AUTH.CALLBACK);
+              // nav(ROUTES.AUTH.CALLBACK);
             },
             onError: (err) => {
               console.error('User signup failed:', err);
@@ -86,7 +85,7 @@ const Signup = () => {
           {
             onSuccess: (res) => {
               console.log('Pro signup success:', res);
-              nav(ROUTES.AUTH.CALLBACK);
+              // nav(ROUTES.AUTH.CALLBACK);
             },
             onError: (err) => {
               console.error('Pro signup failed:', err);
