af5161c6f17f7f8388802fad2405cfe856509723
leetaesk <itasuk@naver.com>
2025-08-18 15:55:10 +0900

    controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import UnreadCountController from '@/features/Chat/controller/UnreadCountController';
+import AlarmController from '@/layout/controller/AlarmController';
+
 import { buildRoutes } from './routes/builder';
 import { routesManifest } from './routes/manifest';
 
@@ -7,6 +10,8 @@ export default function App() {
   const router = createBrowserRouter(buildRoutes(routesManifest));
   return (
     <>
+      <AlarmController />
+      <UnreadCountController />
       <RouterProvider router={router} />
     </>
   );
