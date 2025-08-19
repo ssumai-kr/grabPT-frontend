504d28b4a401ebea1c9555c7f410a0b9b5809213
Starving_Orange <kyungjjoo@gmail.com>
2025-08-19 22:52:14 +0900

    매칭 현황 종목 업데이트

--- src/pages/Home/UserMainPage.tsx
+++ src/pages/Home/UserMainPage.tsx
@@ -4,15 +4,30 @@ import 'slick-carousel/slick/slick.css';
 
 import Banner from '@/components/Banner';
 import RealtimeMatchingStatus from '@/components/RealtimeMatchingStatus';
+import { SPORTS } from '@/constants/sports';
 import RequestSlider from '@/features/home/components/RequestSlider';
 import UserSearchSection from '@/features/home/components/UserSearchSection';
 import { useGetMyRequestsList } from '@/hooks/useGetMyRequestsList';
+import { useGetUserInfo } from '@/hooks/useGetUserInfo';
 import { useRoleStore } from '@/store/useRoleStore';
+import type { SportsSlugType } from '@/types/SportsType';
 
 const UserMainPage = () => {
-  const { isLoggedIn } = useRoleStore();
+  const { isLoggedIn, role } = useRoleStore();
   const { data: requests } = useGetMyRequestsList({ page: 1, size: 40 }, isLoggedIn);
-
+  // 랜덤 선택 함수
+  function getRandomSportSlug(): SportsSlugType {
+    const randomIndex = Math.floor(Math.random() * SPORTS.length);
+    return SPORTS[randomIndex].slug;
+  }
+  const { data: userData } = useGetUserInfo();
+  const matched = SPORTS.find((s) => s.slug === userData?.categoryName);
+  const categoryType: SportsSlugType =
+    role === 'GUEST'
+      ? getRandomSportSlug()
+      : role === 'USER'
+        ? (matched?.slug ?? 'health')
+        : 'health'; // fallback
   return (
     <div className="flex flex-col items-center justify-center">
       <UserSearchSection />
@@ -22,9 +37,9 @@ const UserMainPage = () => {
           <RequestSlider title={'나의 요청서'} requests={requests?.content ?? []} />
         </div>
       )}
-
+      {}
       <div className="my-[200px]">
-        <RealtimeMatchingStatus categoryType={'health'} />
+        <RealtimeMatchingStatus categoryType={categoryType} />
       </div>
 
       <div className="mx-auto mb-[200px] max-w-[1480px] sm:w-[720px] lg:w-[720px] xl:w-[1080px] 2xl:w-[1480px]">
