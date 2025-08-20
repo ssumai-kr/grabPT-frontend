cd2f243c8594aa9b796a1d4714e7885f42130943
leetaesk <itasuk@naver.com>
2025-08-20 14:24:45 +0900

    fix

--- src/pages/Contracts/ContractDetailPage.tsx
+++ src/pages/Contracts/ContractDetailPage.tsx
@@ -5,7 +5,7 @@ import LoadingMuscle from '@/components/LoadingMuscle';
 import { useGetContractPdf } from '@/features/Contract/hooks/useGetContractPdf';
 
 const ContractDetailPage = () => {
-  const id = useParams();
+  const { id } = useParams();
   const contractId = Number(id);
   const { data: pdfUrl, isError, isPending } = useGetContractPdf({ contractId });
   if (isPending) return <LoadingMuscle />;
