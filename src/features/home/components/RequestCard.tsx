3346bb8811e85834a6940a1d7758e0ed0918501d
Starving_Orange <kyungjjoo@gmail.com>
2025-08-20 20:26:52 +0900

    요청서 슬라이더 ui 수정

--- src/features/home/components/RequestCard.tsx
+++ src/features/home/components/RequestCard.tsx
@@ -82,28 +82,19 @@ const RequestCardInMain = ({
       onClick={() => navigate(urlFor.requestDetail(id))}
       className="flex h-[220px] max-w-[340px] cursor-pointer flex-col gap-[12px] rounded-xl px-[10px] py-[15px] shadow-[4px_4px_10px_rgba(0,0,0,0.25)] transition-transform duration-200 hover:scale-[1.02] lg:w-[320px] xl:w-[320px] 2xl:w-[340px]"
     >
-      <div
-        className={clsx(
-          'mx-1.5 ml-auto flex w-16 items-center justify-center rounded-2xl py-1',
-          isMatched
-            ? 'bg-gradient-to-bl from-[#66BB6A] to-[#43A047] shadow-2xs'
-            : 'bg-gradient-to-bl from-[#FF9800] to-[#F57C00] shadow-2xs',
-        )}
-      >
+      <div className="flex items-center justify-between">
         <div
           className={clsx(
             'h-[13px] w-[13px] rounded-full',
-            isMatched
-              ? 'bg-gradient-to-b from-[#00FF00] to-[#006400]'
-              : 'bg-gradient-to-b from-[#FFB74D] to-[#E65100]',
+            isMatched ? 'bg-[#4CAF50]' : 'bg-[#FF8A00]',
           )}
         />
         {/* 상태 텍스트 */}
-        <p className="font-pretendard ml-[6px] text-[12px] leading-[16.8px] font-medium text-white">
+        <p className="font-pretendard ml-[6px] text-[12px] leading-[16.8px] font-medium text-[#000]">
           {isMatched ? '매칭 성공' : '대기중'}
         </p>
+        <UserRequestHeader nickName={name} location={location} profileImg={profileImg} />
       </div>
-      <UserRequestHeader nickName={name} location={location} profileImg={profileImg} />
       <div className="flex flex-wrap gap-[6px]">
         {tagsResult.map((tag, idx) => (
           <Hashtag key={idx} tag={tag} />
