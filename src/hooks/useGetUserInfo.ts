16095f7810530e68a4e194f7a25083c639bb5bbe
Starving_Orange <kyungjjoo@gmail.com>
2025-08-20 17:22:51 +0900

    메인 페이지 컴포넌트 로직 리팩터링

--- src/hooks/useGetUserInfo.ts
+++ src/hooks/useGetUserInfo.ts
@@ -1,18 +1,15 @@
-// src/hooks/useGetUserInfo.ts
 import { useQuery } from '@tanstack/react-query';
 
 import { type UserInfo, getUserInfo } from '@/apis/getUserInfo';
-import { useRoleStore } from '@/store/useRoleStore';
 
 export function useGetUserInfo() {
-  const { role } = useRoleStore();
   return useQuery<UserInfo>({
     queryKey: ['userInfo'],
     queryFn: async () => {
       const data = await getUserInfo();
       return data.result;
     },
-    enabled: role === 'USER',
+    enabled: true,
     staleTime: 1000 * 60 * 5,
   });
 }
--- src/layout/components/AuthMenu.tsx
+++ src/layout/components/AuthMenu.tsx
@@ -7,7 +7,6 @@ import Chat from '@/assets/images/Chat.png';
 import HeaderProfile from '@/assets/images/HeaderProfile.png';
 import Button from '@/components/Button';
 import ROUTES from '@/constants/routes';
-import { useProProfileQuery } from '@/hooks/useGetProProfile';
 import { useGetUserInfo } from '@/hooks/useGetUserInfo';
 import AlarmDropdown from '@/layout/components/AlarmDropdown';
 import ProfileDropdown from '@/layout/components/ProfileDropdown';
@@ -26,13 +25,7 @@ function AuthMenu() {
   const alarmCount = useAlarmStore((s) => s.alarmCount);
 
   const { data: myInfo } = useGetUserInfo();
-  const { data: proInfo } = useProProfileQuery();
-  const profileImage =
-    role === 'USER'
-      ? myInfo?.profileImageUrl
-      : role === 'EXPERT'
-        ? proInfo?.profileImageUrl
-        : HeaderProfile;
+  const profileImage = myInfo?.profileImageUrl ?? HeaderProfile;
 
   //url 변경될때마다 드롭다운 닫기
   useEffect(() => {
