0d2c4b9b520ce1c2a13f36f634c847e5eae3f377
leetaesk <itasuk@naver.com>
2025-08-21 15:33:58 +0900

    alarmdropdown 없을때

--- src/layout/components/AlarmDropdown.tsx
+++ src/layout/components/AlarmDropdown.tsx
@@ -8,7 +8,7 @@ const AlarmDropdown = () => {
   return (
     <Box width="w-[300px]" height="h-[330px]" className="bg-white">
       <div className="my-2 flex w-full [transform:translateZ(0)] flex-col gap-2 overflow-y-scroll pr-1.5 pl-4 [will-change:transform] [contain:layout_paint]">
-        {alarmList ? (
+        {alarmList !== undefined && alarmList.length !== 0 ? (
           alarmList.map((alarm) => {
             return <AlramDropdownItem alarm={alarm} key={alarm.id} />;
           })
