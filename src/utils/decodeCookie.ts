23e483eaf324de8fcf70f7f96929ea771d4ec96e
ssumai-kr <>
2026-01-26 07:18:12 +0000

    fix:console

--- src/utils/decodeCookie.ts
+++ src/utils/decodeCookie.ts
@@ -1,10 +1,7 @@
 import { decodeBase64Utf8 } from '@/utils/decodeBaseUtf8';
 
 export function decodeCookie(name: string) {
-  const getCookieValue = (name: string) => {
-    const match = document.cookie.split('; ').find((row) => row.startsWith(name + '='));
-    return match ? match.split('=')[1] : '';
-  };
-
-  return decodeBase64Utf8(getCookieValue(name));
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  const value = match ? match[2] : '';
+  return decodeBase64Utf8(value);
 }
